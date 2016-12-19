0bdf9c8dbd80ee2bcc7219ca49a3fd4fc6d1f7f6
Aniruddha Chakraborty <achakraborty510@gmail.com>
2016-12-19 23:28:07 +0600

    added validator function

--- lib/validator.py
+++ lib/validator.py
@@ -1,30 +1,95 @@
 import os
 import sys
 from flask import request
+import re
 
 class Validator:
 	def __init__():
-		pass
+		self.requireError = False
+		self.maxError 	  = False
+		self.minError 	  = False
+		self.emailError   = False
+		self.integerError = False
+		self.stringError  = False
 
-	def require(value):
+
+	def validate(dictonary):
+		if type(dictonary) not dict:
+			raise ValueError('please pass dictionary')
+
+		for dictionary in dictionary:
+			print(dictionary)
+
+	#check for required
+	def require(self,value,errorMessage = ''):
+
+		if errorMessage:
+			self.requireError = errorMessage
+
+
 		if value == "":
-			return False
+			return self.requireError
 		else:
-			return True
+			return ''
 
-	def max(value,max):
+	#set maximum value
+	def max(self,value,max,errorMessage = ''):
 		try:
 			val = int(value)
+			self.maxError = errorMessage
+
 			if (val > max ):
-				return False
+				return self.maxError
+			else:
+				return ''
 
 		except ValueError:
-			print('it\'s not an integer')
+			print("it's not an integer")
 
-	def min(value,min):
+	#set minimum value
+	def min(self,value,min,errorMessage = ''):
 		try:
 			val = int(value)
+
+			if errorMessage:
+				self.minError = errorMessage
+
 			if val < min:
-				return False
+				return self.minError
+			else:
+				return ''
 
 		except ValueError:
+			print("It's not an integer")
+
+	#check for input that email or not
+	def email(self,value,errorMessage = ''):
+		value = re.group("\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b")
+
+		if errorMessage:
+			self.emailError = errorMessage
+
+		if value == "":
+			return self.emailError
+		else:
+			return ''
+
+	#check for integer value
+	def integer(self,value,errorMessage =''):
+		if errorMessage:
+			self.integerError = errorMessage
+
+		if type(value) not int:
+			return self.integerError
+		else:
+			return ''
+
+	#check for string value
+	def string(self,value,errorMessage = ''):
+		if errorMessage:
+			self.stringError = errorMessage
+
+		if type(value) not str:
+			return self.stringError
+		else:
+			return ''

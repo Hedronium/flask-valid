e600feec133055ce37dc590339c1c504c240151e
Aniruddha Chakraborty <achakraborty510@gmail.com>
2016-12-22 01:01:58 +0600

    updated validator class added csrf token validator

--- lib/validator.py
+++ lib/validator.py
@@ -1,24 +1,28 @@
 import os
 import sys
-from flask import request
+from flask import request,session
 import re
+import hashlib
+import random
 
 class Validator:
-	def __init__():
+	def __init__(self,dictionary):
 		self.requireError = False
 		self.maxError 	  = False
 		self.minError 	  = False
 		self.emailError   = False
 		self.integerError = False
 		self.stringError  = False
+		self.validate(dictionary)
 
 
-	def validate(dictonary):
-		if type(dictonary) not dict:
-			raise ValueError('please pass dictionary')
+	def validate(self,dictionary):
+		if type(dictionary) is dict:
+			for diction in dictionary:
+				print(diction)
+		else:	
+			raise ValueError('Please pass a dicitonary')
 
-		for dictionary in dictionary:
-			print(dictionary)
 
 	#check for required
 	def require(self,value,errorMessage = ''):
@@ -79,7 +83,7 @@ class Validator:
 		if errorMessage:
 			self.integerError = errorMessage
 
-		if type(value) not int:
+		if type(value) is not int:
 			return self.integerError
 		else:
 			return ''
@@ -89,7 +93,28 @@ class Validator:
 		if errorMessage:
 			self.stringError = errorMessage
 
-		if type(value) not str:
+		if type(value) is not str:
 			return self.stringError
 		else:
 			return ''
+
+	#generate csrf token
+	def csrftoken(self): 
+		self.csrfdestroy()
+		hashed_data = str(random.randrange(99999))
+		hashed_data = hashlib.sha224(bytes(hashed_data, encoding='utf-8')).hexdigest()
+		session['sdfshdfahlfgfdlhdfkjghakgjhdfkgjdfhgdfgnjgnf'] = hashed_data
+		return hashed_data
+
+	#get csrf value
+	def getcsrf(self):
+		data = session.get('sdfshdfahlfgfdlhdfkjghakgjhdfkgjdfhgdfgnjgnf')
+		self.csrfdestroy()
+		return data
+
+	def csrffiled(self):
+		return "<input type=\"hidden\" value=\""+ self.csrftoken() +"\""
+
+	def csrfdestroy(self):
+		return session['sdfshdfahlfgfdlhdfkjghakgjhdfkgjdfhgdfgnjgnf'] = ''
+
